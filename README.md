28552ea92bad8800dc0642c05bdf0a951e82603d
zhaoxianghui <zhaoxianghui@baidu.com>
2018-04-18 00:13:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+执行：python flask-demo.py
+
+访问：127.0.0.1:5000
---
+++ flask-demo.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+import os
+from flask import Flask, render_template, url_for, make_response, send_from_directory
+
+# 生成Flask实例
+app = Flask(__name__)
+
+
+@app.route('/')
+def test():
+    print('get')
+    return render_template('candlestick-brush.html')
+
+
+@app.route("/download/<filename>", methods=['GET'])
+def download_file(filename):
+    directory = '/'
+    print(directory)
+    return send_from_directory(directory, filename, as_attachment=True)
+
+
+if __name__ == "__main__":
+    # 运行项目
+    app.run(debug=True)
