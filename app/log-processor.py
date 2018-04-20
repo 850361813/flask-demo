3dcabc7bbeeb437879f8a2feb6a2cafd7bb70783
zhaoxianghui <zhaoxianghui@baidu.com>
2018-04-20 21:40:07 +0800

    update

--- app/log-processor.py
+++ app/log-processor.py
@@ -1,5 +1,21 @@
 # -*- coding:utf-8 -*-
 
+LOG_FILE = '../sources/log1.txt'
 
-if __name__ == '__main__':
-    pass
+
+# 卖开
+# 买平今
+# 卖平今
+# 卖开
+# 撤单
+
+
+def load_log(log_file):
+    log_data = []
+    file = open(log_file)
+    for line in file:
+        if '买开' in line or '买平今' in line or '卖平今' in line or '卖开' in line or '撤单' in line:
+            log_data.append(line.split(' '))
+    file.close()
+    return log_data
+
--- flask-app.py
+++ flask-app.py
@@ -4,7 +4,8 @@ from flask import Flask, render_template, url_for, make_response, send_from_dire
 
 # 生成Flask实例
 app = Flask(__name__)
-
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
+APP_STATIC_TXT = os.path.join(APP_ROOT, 'sources') #设置一个专门的类似全局变量的东西
 
 @app.route('/')
 def test():
@@ -17,6 +18,17 @@ def download_file(filename):
     directory = './sources/'
     print(directory)
     return send_from_directory(directory, filename, as_attachment=True)
+
+
+@app.route("/download/log", methods=['GET'])
+def download_log_file():
+    log_data = []
+    file = open(os.path.join(APP_STATIC_TXT, 'log1.txt'))
+    for line in file:
+        if '买开' in line or '买平今' in line or '卖平今' in line or '卖开' in line or '撤单' in line:
+            log_data.append(line.split(' '))
+    file.close()
+    return str(log_data)
 
 
 if __name__ == "__main__":
